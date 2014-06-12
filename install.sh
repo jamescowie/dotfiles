87737ce9dda06c0d51aa9356b1891e256334522c
James Cowie <jcowie@inviqa.com>
2014-06-12 20:32:54 +0100

    Add hook templates for git

--- install.sh
+++ install.sh
@@ -2,9 +2,15 @@
 
 echo "Installing Jcowie DotFiles"
 
-DOTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+DOTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ln -nfs $DOTDIR/.gitconfig ~/
 ln -nfs $DOTDIR/.githelpers ~/
+ln -nfs $DOTDIR/.gitignore_global ~/
 ln -nfs $DOTDIR/fish/config.fish ~/.config/fish/
 ln -nfs $DOTDIR/.osx ~/
+ln -nfs $DOTDIR/git ~/
+
+ln -nfs $DOTDIR/bin/bdd ~/bdd
+ln -nfs $DOTDIR/bin/spec ~/spec
+ln -nfs $DOTDIR/bin/symf ~/symf
