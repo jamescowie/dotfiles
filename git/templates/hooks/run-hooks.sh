de69bdac8760a217c1ff0bbd732fef44b7452025
jamescowie <james@jcowie.co.uk>
2014-06-23 23:00:43 +0100

    Remove duplicate information in hook, make phpcs hook use colors

--- git/templates/hooks/run-hooks.sh
+++ git/templates/hooks/run-hooks.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/bin/zsh
+
+COLOR_RED="\x1B[31m"
 
 EXIT_CODE=0
 
@@ -6,17 +8,17 @@ repo=$( git rev-parse --show-toplevel )
 hook_type=$( basename $0 )
 hooks=~/git/hooks
 
-echo "Executing $hook_type hook(s)"
+#echo "Executing $hook_type hook(s)"
 
 for hook in $hooks/*.$hook_type; do
-	echo ""
-	echo "${COLOR_LIGHTPURPLE}Executing ${hook}${COLOR_NONE}"
+#	echo ""
+#	echo "${COLOR_RED}Executing ${hook}${COLOR_NONE}"
 	${hook}
 	EXIT_CODE=$((${EXIT_CODE} + $?))
 done
 
 if [[ ${EXIT_CODE} -ne 0 ]]; then
-	echo ""
+#	echo ""
 	echo "${COLOR_RED}Commit Failed.${COLOR_NONE}"
 fi
 
