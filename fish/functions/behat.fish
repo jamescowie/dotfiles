0cabeea6a563a907590218dd2c7d2a1da30afb7e
jamescowie <james@jcowie.co.uk>
2014-06-29 21:24:02 +0100

    Add check to install behat when behat is called

--- fish/functions/behat.fish
+++ fish/functions/behat.fish
@@ -2,7 +2,15 @@ function behat --description '±Behat shortcut'
     if test -e ./bin/behat
         ./bin/behat $argv
     else
-        echo 'Whoops Behat is not installed. Are you in a project ?'
+        echo 'Whoops Behat is not installed.'
+        if test -f "composer.json"
+            set_color orange
+            echo 'Shall we install behat for you ? [yN]'
+            read action
+            if contains "Y" $action
+                composer require 'behat/behat *'
+            end
+        end
     end
 end
 
