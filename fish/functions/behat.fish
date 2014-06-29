89621ee7039920f8b07a99501f88a47e45a17c1c
jamescowie <james@jcowie.co.uk>
2014-06-29 21:27:30 +0100

    Fix installation of behat when running and composer is ready

--- fish/functions/behat.fish
+++ fish/functions/behat.fish
@@ -2,12 +2,13 @@ function behat --description '±Behat shortcut'
     if test -e ./bin/behat
         ./bin/behat $argv
     else
+        set_color red
         echo 'Whoops Behat is not installed.'
         if test -f "composer.json"
-            set_color orange
+            set_color purple
             echo 'Shall we install behat for you ? [yN]'
             read action
-            if contains "Y" $action
+            if contains "y" $action
                 composer require 'behat/behat *'
             end
         end
